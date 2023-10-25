4bf92bac30052a898bee8e4a7e8178d13c6375fa
allanisPixel <alanisisabelle@gmail.com>
2023-10-25 18:56:14 -0300

    Arrumei a tela de splash

--- bestiario/lib/page_1.dart
+++ bestiario/lib/page_1.dart
@@ -8,43 +8,21 @@ class MeuApp1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(scaffoldBackgroundColor: Color.fromARGB(255, 0, 0, 0)),
-      home: Scaffold(
-        
-        body: const Center(
+      theme: ThemeData(
+          scaffoldBackgroundColor: Color.fromARGB(255, 0, 0, 0),
+          textTheme: Typography.whiteCupertino),
+      home: const Scaffold(
+          body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            
-          ///////
-          //Container(
-          //constraints: BoxConstraints.expand(
-          //  height: Theme.of(context).textTheme.headlineMedium!.fontSize! * 1.1 + 200.0,
-          //),
-          //padding: const EdgeInsets.all(8.0),
-          //color: Color(0xFFFFFF),
-          //alignment: Alignment.center,
-          //child: Text('Hello World',
-          //  style: Theme.of(context)
-          //      .textTheme
-          //      .headlineMedium!
-          //      .copyWith(color: Colors.white)),
-          //),
-          //////////////
-
-
-
-
-
-
-            
             Image(
               image: AssetImage('assets/images/logo.png'),
             ),
             Text(
               'Bestiario PT/BR',
               textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
               selectionColor: Color.fromARGB(255, 255, 255, 255),
             )
           ],
